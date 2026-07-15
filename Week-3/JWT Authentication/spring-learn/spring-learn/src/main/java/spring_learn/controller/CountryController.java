0fcc776ba7f19ca97d8b7a182843ce6955306967
Hemavarshini S <hemavarshinis.23aid@kongu.edu>
2026-07-15 09:44:07 +0530

    Added JWT-Handson Authentication

---
+++ Week-3/JWT Authentication/spring-learn/spring-learn/src/main/java/spring_learn/controller/CountryController.java
@@ -0,0 +1,27 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    private final CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        LOGGER.info("START");
+        Country country = countryService.getCountry(code);
+        LOGGER.info("END");
+        return country;
+    }
+}
